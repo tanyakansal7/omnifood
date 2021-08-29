fcb58b8acbf0d0d7ceaeddfc5eb976bc30fdf6be
tanyakansal7 <you@example.com>
2021-08-29 16:15:49 +0530

    a static website

---
+++ resources/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    
+// This is for the sticky navigation
+    $('.js--section-features').waypoint(function(direction){
+        if(direction=="down"){
+            $('nav').addClass('sticky');
+        }else{
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+    
+   
+    
+//    Scroll on buttons
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    
+});
